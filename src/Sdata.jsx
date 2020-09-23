9e99243dc37b7ee57a3826a88cfd8e19cd0b25b0
mrigankx <mrigankagoswami01@gmail.com>
2020-09-23 22:31:54 +0530

    react-create-app on Heroku

---
+++ src/Sdata.jsx
@@ -0,0 +1,27 @@
+const Sdata = [
+  {
+    sname: "Mirazapur",
+    imgsrc:
+      "https://upload.wikimedia.org/wikipedia/en/3/3c/Poster_of_Mirzapur_2018.jpg",
+    title: "An Amazon Prime Series",
+    link:
+      "https://www.primevideo.com/detail/Mirzapur/0PDOKMV9CRLOMO5EUKNCUJLG4Q",
+  },
+  {
+    sname: "Patal Lok",
+    imgsrc:
+      "https://upload.wikimedia.org/wikipedia/en/3/39/Paatal_Lok_poster.jpg",
+    title: "An Amazon Prime Series",
+    link:
+      "https://www.primevideo.com/detail/0G9IEOHCN8KMY6COD9ILGH7IY5/ref=atv_hm_hom_c_bV3MWc_hnDIaR_1_1",
+  },
+  {
+    sname: "Money Heist",
+    imgsrc:
+      "https://images-na.ssl-images-amazon.com/images/I/61boFr6SYZL._SL1000_.jpg",
+    title: "A Netflix Series",
+    link: "https://www.netflix.com/title/80192098",
+  },
+];
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Card.jsx";
+import "./index.css";
+import Sdata from "./Sdata";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <React.Fragment>
+    <h1 className="heading_style">My Favourite Shows</h1>
+    {Sdata.map((val) => {
+      return (
+        <Card
+          imgsrc={val.imgsrc}
+          title={val.title}
+          sname={val.sname}
+          link={val.link}
+        />
+      );
+    })}
+    ;
+  </React.Fragment>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
